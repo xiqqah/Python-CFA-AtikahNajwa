82217c89101e89a1188fa44da28f881436ed3aef
xiqqah <136549393+xiqqah@users.noreply.github.com>
2023-06-14 11:57:55 +0800

    Add files via upload

---
+++ guessing game.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+#Guessing game
+import random
+
+def main():
+    #Game explanation
+    """Start a music genre guessing game."""
+    print("Guess The Music Genre!")
+
+    #Keep track of guesses
+    guess_count = 0
+    guess_limit = 3
+
+    print("===============================================================================================")
+
+    #Show a list of choices
+    a = [ 'Pop Music' ,'Funk Music' ,'Rock Music' ,'Kpop Music' ,'Jpop Music' ,'Country Music' ,'Instrumental Music' ]
+    print("List of music genre you can guess from:",*a, sep = "\n")
+
+    #Game Variables
+    music_genre =[
+        "Pop Music",
+        "Funk Music",
+        "Rock Music",
+        "Kpop Music",
+        "Jpop Music",
+        "Country Music",
+        "Instrumental Music"  
+         ]
+    
+    print("===============================================================================================","\n")
+
+    
+       
+    x = random.choice(music_genre,)
+    guess = None
+
+    #Game engine
+    while x != guess:
+
+        guess = str((input("What music genre am i?: " )))
+
+        #Hints for music genre
+        if x == 'Pop Music':
+            print("hint:has catchy melody and lyrics")
+        elif x == 'Funk Music':
+            print("hint:has strong bass lines,or music lines played by low-pitched instruments and has a heavy syncopated")
+        elif x == 'Rock Music':
+            print("hint:music that emerged in the 1950s and is defined as a form of music with a strong beat")
+        elif x == 'Kpop Music':
+            print("hint:has cathcy melody and intense synchronized choreography accompanied with equally intense visuals")
+        elif x == 'Jpop Music':
+            print("hint:a combination of traditional Japanese music and Western pop music")
+        elif x == 'Country Music':
+            print("hint:honky-tonk music with simple form, folk lyrics and harmonies accompied by instruments as banjos and fiddles")
+        else:
+            print("hint:it just involves instruments with no singing")
+
+        #The ouput for player guesses
+        if x == guess:
+            print("Congratulation! I am {}. You got it right :].".format(guess))
+            break
+        else:
+            print("Oops...I am not {}.Try again!".format(guess))
+
+main()
